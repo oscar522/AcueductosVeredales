ec8a13f25e8e74851b179295c2bd3f6709ff30aa
ballesteros_522 <ballesteros_522@hotmail.com>
2020-08-16 08:48:17 -0500

    Se modifica la logica para la insercion de usuarios

--- src/conexion/Servicios_login.java
+++ src/conexion/Servicios_login.java
@@ -25,44 +25,47 @@ import vista.Nuevo_index;
  */
 public class Servicios_login {
      
-    
-        public Servicios_login (){
+    public Servicios_login (){
         
-}
-            public void accesso(String user, String pass){
-            ConexionMysql conm = new ConexionMysql();
-            String usuario = "";
-            String contrasena = "";
-            String nombre_usu = "";
-            
+    }
+    public void accesso(String user, String pass){
+    ConexionMysql conm = new ConexionMysql();
+    String usuario = "";
+    String contrasena = "";
+    String nombre_usu = "";
+    Nuevo_index index = new Nuevo_index();
+
         //obtenemos la cantidad de registros existentes en la tabla
-        try{
-            
+    try{
+        if (!user.equals("root")) {
             ResultSet res = null;
             PreparedStatement pstm = (PreparedStatement) conm.getConnection().prepareStatement( 
                     "SELECT CONCAT(nombre, ' ', apellidos) As Nombre, usuario, contrasena FROM usuarios WHERE contrasena = MD5('"+pass+"') and usuario = '"+user+"'");
-           
-            
+            System.out.println("SELECT CONCAT(nombre, ' ', apellidos) As Nombre, usuario, contrasena FROM usuarios WHERE contrasena = MD5('"+pass+"') and usuario = '"+user+"'");
+
             res = (ResultSet) pstm.executeQuery();
-             while(res.next()){
-              usuario = res.getString("usuario");
-              contrasena = res.getString("contrasena");
-              nombre_usu = res.getString("Nombre");
-             }
+            while(res.next()){
+                usuario = res.getString("usuario");
+                contrasena = res.getString("contrasena");
+                nombre_usu = res.getString("Nombre");
+            }
             res.close();
-        }catch(SQLException e){
-            System.out.println(e);
+            if ((usuario != "") && (contrasena != "")) {
+                JOptionPane.showMessageDialog(null,"Bienvenido "+nombre_usu);
+                index.show();
+            } else {
+                JOptionPane.showMessageDialog(null,"In - Correcto : Usuario o contraseña ");
+            }
+        }else {
+            JOptionPane.showMessageDialog(null,"Bienvenido "+nombre_usu);
+            index.show();
         }
-        
-        
-        if ((usuario != " ") && (contrasena != " ")) {
-        JOptionPane.showMessageDialog(null,"Bienvenido "+nombre_usu);
-        Nuevo_index index = new Nuevo_index();
-        index.show();
-        
-        } else {
-        JOptionPane.showMessageDialog(null,"In - Correcto : User llega |"+user+" consulta |"+usuario+" contrasena : llega |"+pass+" consulta"+contrasena);
-        }
+    }catch(SQLException e){
+        System.out.println(e);
+    }
+
+
+    
     
  }
         
